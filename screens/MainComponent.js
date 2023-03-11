699825e9886bd42c48868c8a1bbe9470f5abbf93
hammervong <119082579+hammervong@users.noreply.github.com>
2023-03-11 12:47:36 -0600

    Workshop 4

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -240,27 +240,23 @@ const Main = () => {
     }, [dispatch]);
 
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
+        showNetInfo();
+    }, []);
+
+        const showNetInfo = async () => {
+            const connectionInfo = await NetInfo.fetch();
             Platform.OS === 'ios'
-                ? Alert.alert(
-                      'Initial Network Connectivity Type:',
-                      connectionInfo.type
-                  )
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
                 : ToastAndroid.show(
-                      'Initial Network Connectivity Type: ' +
-                          connectionInfo.type,
+                      'Initial Network Connectivity Type: ' + connectionInfo.type,
                       ToastAndroid.LONG
                   );
-        });
-
-        const unsubscribeNetInfo = NetInfo.addEventListener(
-            (connectionInfo) => {
+            const unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
                 handleConnectivityChange(connectionInfo);
-            }
-        );
+            });
 
-        return unsubscribeNetInfo;
-    }, []);
+            return unsubscribeNetInfo;
+        };
 
     const handleConnectivityChange = (connectionInfo) => {
         let connectionMsg = 'You are now connected to an active network.';
